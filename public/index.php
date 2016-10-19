d90e3598e0da9eab6958b8a8d24a14d797c51f48
Byron <bgva2005@yahoo.com>
2016-10-18 22:17:01 -0500

    Valida inclusion de require_once y visualiza con print_t

---
+++ public/index.php
@@ -0,0 +1,27 @@
+
+<?php
+	require_once 'config.php';
+	require_once 'db.php';
+	require_once 'paginador.php';
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<pre>
+	<?php
+		print_r(get_required_files());
+
+	?>
+	
+	
+	</pre>
+	
+</body>
+</html>
